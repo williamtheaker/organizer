26594335673a1f2764494acea300b3f305c7a312
Torrie Fischer <tdfischer@hackerbots.net>
2017-05-30 15:08:18 -0700

    organizer: urls: add a catch-all route

--- organizer/urls.py
+++ organizer/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from crm import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('crm.api_urls')),
     url(r'^crm/', include('crm.urls')),
+    url(r'^', views.index),
 ]
