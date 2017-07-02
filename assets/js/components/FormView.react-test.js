20ded07ca71ea3e696cd2ccde535a98767b12a59
Torrie Fischer <tdfischer@hackerbots.net>
2017-07-02 14:19:25 -0700

    app: add app manifest

--- assets/js/components/FormView.react-test.js
+++ assets/js/components/FormView.react-test.js
@@ -1,6 +1,6 @@
 import fetchMock from 'fetch-mock'
 import React from 'react'
-import FormView, { SignupForm, Thanks, FormInputView } from './FormView'
+import FormView, { FormInputView, SignupForm, Thanks } from './FormView'
 import Spinner from './Spinner'
 import { Form } from 'react-form'
 import { Form as ModelForm } from '../Model'
@@ -64,16 +64,13 @@ describe('FormView', () => {
   }
 
   it('should load data if one is not embedded in the page', () => {
-    delete global.INLINE_FORM_DATA;
+    global.INLINE_FORM_DATA = undefined;
     fetchMock.getOnce('/api/forms/1/', {
       status: 200
-    })
+    });
     const component = mount(<Router><ThemeProvider><FormView match={urlMatch} /></ThemeProvider></Router>);
     return fetchMock.flush().then(() => {
       expect(fetchMock.called()).toEqual(true);
-      expect(component.find(Thanks)).toHaveLength(0);
-      expect(component.find(FormInputView)).toHaveLength(0);
-      expect(component.find(Spinner)).toHaveLength(1);
     })
   })
 
@@ -82,16 +79,28 @@ describe('FormView', () => {
     fetchMock.getOnce('/api/forms/1/', {
       status: 200
     })
-    const component = mount(<Router><ThemeProvider><FormView match={urlMatch} /></ThemeProvider></Router>);
+    const component = shallow(<Router><ThemeProvider><FormView match={urlMatch} /></ThemeProvider></Router>);
     return fetchMock.flush().then(() => {
       expect(fetchMock.called()).toEqual(false);
-      expect(component.find(Thanks)).toHaveLength(0);
-      expect(component.find(FormInputView)).toHaveLength(1);
-      expect(component.find(Spinner)).toHaveLength(0);
     })
   })
 
-  it('should show the happy house after successful submission', () => {
+  it('should show the spinner while loading', () => {
+    global.INLINE_FORM_DATA = undefined;
+    fetchMock.getOnce('/api/forms/1/', () => new Promise(() => {}))
+    var component = shallow(<FormView match={urlMatch} />);
+    expect(component.find(Thanks)).toHaveLength(0);
+    expect(component.find(Spinner)).toHaveLength(1);
+    expect(component.find(FormInputView)).toHaveLength(0);
+
+    global.INLINE_FORM_DATA = {id: 1};
+    var component = shallow(<FormView match={urlMatch} />);
+    expect(component.find(Thanks)).toHaveLength(0);
+    expect(component.find(Spinner)).toHaveLength(0);
+    expect(component.find(FormInputView)).toHaveLength(1);
+  });
+
+  it('should show the happy house and allow submitting another after successful submission', () => {
     global.INLINE_FORM_DATA = {id: 1};
     fetchMock.postOnce('/api/forms/1/submit_response/', {
       status: 200
@@ -103,8 +112,23 @@ describe('FormView', () => {
     return fetchMock.flush().then(() => {
       expect(fetchMock.called()).toEqual(true);
       expect(component.find(Thanks)).toHaveLength(1);
-      expect(component.find(FormInputView)).toHaveLength(0);
+      expect(component.find(SignupForm)).toHaveLength(0);
+      expect(component.find(Spinner)).toHaveLength(0);
+
+      component.find(Thanks).find('a').first().simulate('click')
+      expect(component.find(Thanks)).toHaveLength(0);
+      expect(component.find(SignupForm)).toHaveLength(1);
       expect(component.find(Spinner)).toHaveLength(0);
     })
   });
 });
+
+describe('Thanks', () => {
+  it('should have an option to submit another response', () => {
+    const handler = jest.fn();
+    const form = new ModelForm();
+    const component = shallow(<Thanks form={form} onSubmitAnother={() => handler()} />)
+    component.find('a').simulate('click');
+    expect(handler).toHaveBeenCalled()
+  })
+})
