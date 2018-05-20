14d06275c787e969e7ea8132fb5a78a22c8a84db
Torrie Fischer <tdfischer@hackerbots.net>
2018-05-19 17:43:44 -0700

    ui: components: appindex: update branding and drop slack login icon

--- assets/js/components/AppIndex.js
+++ assets/js/components/AppIndex.js
@@ -1,25 +1,22 @@
 import React from 'react'
-import { Divider, Paper } from 'material-ui'
+import { SvgIcon, RaisedButton, Divider, Paper } from 'material-ui'
+import EBFESymbol from 'svg-react-loader!../../img/symbol.svg'
 
 export default (props) => (
   <Paper zDepth={1} className="app-splash">
-    <h1>East Bay Forward <div className="pop">Organizer</div></h1>
+    <h1>East Bay for Everyone <div className="pop">Organizer</div></h1>
     <p>The housing shortage is not an unintended policy failure. The Bay Area
       has a housing shortage because of decades of voting and organizing against
       housing. The solution is to organize for housing.</p>
-    <p>East Bay Forward Organizer helps people organize for housing.</p>
+    <p>East Bay for Everyone Organizer helps people organize for housing.</p>
     <Divider />
     <p />
-    <p><a href={SLACK_LOGIN_URL}>
-      <img
-        alt="Sign in with Slack"
-        height="40"
-        width="172"
-        src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-        srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+    <RaisedButton
+      href={SLACK_LOGIN_URL}
+      label="Sign in with EBFE Discuss"
+      icon={<EBFESymbol style={{width: "auto", "height": "80%"}}/>}
       />
-    </a></p>
-    <p>Please sign in with East Bay Forward Slack to continue.</p>
+    <p>Please sign in with East Bay for Everyone Discuss to continue.</p>
     <p className="github-link"><a href="https://github.com/tdfischer/organizer/"><i className="fa fa-github"/></a></p>
   </Paper>
 )
